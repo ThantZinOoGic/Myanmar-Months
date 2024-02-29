bb1c8e789b944593647000a00138aaaa05015c8c
ThantZinOoGic <thantzinoo@gicjp.com>
2024-02-29 16:58:56 +0630

    add bg

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App ">
+    <div className="App " style={{ background : "#bfbfbf" }}>
       <Router>
         <Header/>
         <Routes>
